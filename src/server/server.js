2b43d145128988f79510f13e5fcf15d01d88524a
Fabian Brundke <fabian_brundke@gmx.net>
2019-05-14 21:18:05 +0200

    Express server now waits for mongodb connection to be established before being listened on

--- src/server/server.js
+++ src/server/server.js
@@ -14,23 +14,24 @@ const mongoDatabaseURL = process.env.DB_URL;
 // initiate mongoose connection
 mongoose.connect(mongoDatabaseURL, { useNewUrlParser: true })
   .then(() => console.log(`Connected to mongodb: ${mongoDatabaseURL}`))
+  .then(
+    // CORS
+    app.use(cors()),
+
+    // bodyParser setup
+    app.use(bodyParser.urlencoded({ extended: true })),
+    app.use(bodyParser.json()),
+
+    // routes(app);
+    app.use('/api', apiRouter),
+
+    // to serve files from production build:
+    app.use(express.static('dist')),
+
+    // app.get('/', (req, res) => res.send('test')),
+
+    // app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username })),
+
+    app.listen(PORT, () => console.log(`Express server is running on port:${PORT}`)),
+  )
   .catch(() => console.error());
-
-// CORS
-app.use(cors());
-
-// bodyParser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// routes(app);
-app.use('/api', apiRouter);
-
-// to serve files from production build:
-app.use(express.static('dist'));
-
-app.get('/', (req, res) => res.send('test'));
-
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-
-app.listen(PORT, () => console.log(`Express server is running on port:${PORT}`));
