0f115a36c785888bb07acb11b1b0d6cdfa922ecf
Fabian Brundke <fabian_brundke@gmx.net>
2019-05-14 22:22:05 +0200

    Some cleanup in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import os from 'os';
 
 import apiRouter from './routes/routes';
 
@@ -22,15 +21,12 @@ mongoose.connect(mongoDatabaseURL, { useNewUrlParser: true })
     app.use(bodyParser.urlencoded({ extended: true })),
     app.use(bodyParser.json()),
 
-    // routes(app);
     app.use('/api', apiRouter),
 
     // to serve files from production build:
     app.use(express.static('dist')),
 
     // app.get('/', (req, res) => res.send('test')),
-
-    // app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username })),
 
     app.listen(PORT, () => console.log(`Express server is running on port:${PORT}`)),
   )
