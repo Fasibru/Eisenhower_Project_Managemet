b9020a389b0e8f5d7f2c0c6b215f3871223216a8
Fabian Brundke <fabian_brundke@gmx.net>
2019-05-27 21:11:24 +0200

    Added more extensive testing for Sidenav component.

--- __tests__/__unit__/Sidenav.test.js
+++ __tests__/__unit__/Sidenav.test.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Sidenav from '../../src/client/components/js/Sidenav';
 
-describe('Testing Sidenav', () => {
-  it('Renders correctly', () => {
-    const sidenav = renderer
-      .create(<Sidenav
-        toggleNewTaskPopup={() => { }}
-        filters={{
-          showTasks: 'both',
-        }}
-        handleFilterShowTasks={() => { }}
-      />);
+describe('Testing <Sidenav />', () => {
+  const fnClick = jest.fn();
+  const fnChange = jest.fn();
+  let wrapper;
 
-    expect(sidenav).toMatchSnapshot();
+  beforeEach(() => {
+    wrapper = shallow(<Sidenav
+      toggleNewTaskPopup={fnClick}
+      filters={{
+        showTasks: 'both',
+      }}
+      handleFilterShowTasks={fnChange}
+    />);
+  });
+
+  it('Renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should populate "select" based on provided props', () => {
+    const select = wrapper.find('select');
+
+    expect(select.props().value).toEqual('both');
+  });
+
+  it('Should call "handleFilterShowTasks" on change', () => {
+    const event = {
+      target:
+      { value: '' },
+    };
+    const select = wrapper.find('select');
+    select.simulate('change', event);
+
+    expect(fnChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should call "toggleNewTaskPopup" on button click', () => {
+    const button = wrapper.find('button');
+    button.simulate('click');
+
+    expect(fnClick).toHaveBeenCalledTimes(1);
   });
 });
