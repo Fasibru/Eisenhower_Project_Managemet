1c4ef41ec1624033dc63f8a69a44f6b5e706b31e
Fabian Brundke <fabian_brundke@gmx.net>
2019-05-27 20:57:41 +0200

    Added more extensive testing for Task component.

--- __tests__/__unit__/Task.test.js
+++ __tests__/__unit__/Task.test.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Task from '../../src/client/components/js/Task';
 
-describe('testing Task', () => {
-  it('renders correctly', () => {
-    const task = renderer
-      .create(
-        <Task
-          className="test"
-          key={1}
-          task={{
-            _id: '2',
-            rank: -999,
-            category: 'A',
-            title: 'Test Task',
-            description: 'Testing Task',
-            completed: false,
-          }}
-          populateEditTask={() => {}}
-        />,
-      ).toJSON();
+describe('Testing <Task />', () => {
+  const fnDoubleClick = jest.fn();
+  let wrapper;
 
-    expect(task).toMatchSnapshot();
+  beforeEach(() => {
+    wrapper = shallow(<Task
+      className="test"
+      key={1}
+      task={{
+        _id: '2',
+        rank: -999,
+        category: 'A',
+        title: 'Task Title',
+        description: 'Task Description',
+        completed: false,
+      }}
+      populateEditTask={fnDoubleClick}
+    />);
+  });
+
+  it('Renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should populate "title" and "description" based on provided props', () => {
+    const pTitle = wrapper.find('p').at(0);
+    const pDescription = wrapper.find('p').at(1);
+
+    expect(pTitle.text()).toEqual('Task Title');
+    expect(pDescription.text()).toEqual('Task Description');
+  });
+
+  it('Should run "populateEditTask" on double click', () => {
+    const div = wrapper.find('div');
+    div.simulate('doubleClick');
+
+    expect(fnDoubleClick).toHaveBeenCalledTimes(1);
   });
 });
