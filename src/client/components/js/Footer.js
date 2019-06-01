a2b01cd52e5cfd06081f01c8ecae7c1a37ee9ad6
Fabian Brundke <fabian_brundke@gmx.net>
2019-06-01 08:37:10 +0200

    Added link to my Github page in Footer

--- src/client/components/js/Footer.js
+++ src/client/components/js/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import '../css/Footer.css';
 
 function Footer() {
   return (
     <footer className="footer">
       <p style={{ color: '#f5fbef' }}>
-        Some metadata
+        <a href="https://github.com/Fasibru" style={{ color: '#f5fbef', textDecoration: 'none' }}>
+          <FontAwesomeIcon icon={faGithub} size="2x" /> Get the repository.
+        </a>
       </p>
     </footer>
   );
