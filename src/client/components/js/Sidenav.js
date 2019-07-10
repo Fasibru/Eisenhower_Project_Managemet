7d6035e7170a206d739321e36267deeb79d4fe99
Fabian Brundke <fabian_brundke@gmx.net>
2019-07-10 21:15:40 +0200

    Extracted filter for task status in Sidenav to seperate component.

--- src/client/components/js/Sidenav.js
+++ src/client/components/js/Sidenav.js
@@ -1,43 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import {
-  updateFilters,
   openNewTaskPopup,
 } from '../../actions/index';
 import '../scss/Sidenav.scss';
 
+import StatusFilter from './StatusFilter';
 import DateFilter from '../../containers/DateFilter.container';
 
-const mapStateToProps = state => ({
-  filters: state.filters.filters,
-});
-
-export function Sidenav(props) {
-  const {
-    // eslint-disable-next-line no-shadow
-    openNewTaskPopup,
-    filters,
-    // eslint-disable-next-line no-shadow
-    updateFilters,
-  } = props;
-
-  const handleFilter = (event) => {
-    // eslint-disable-next-line no-shadow
-    updateFilters(event.target.name, event.target.value);
-
-    axios.put('/api/filters', {
-      ...filters,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const classNameButtonActive = 'sidenav__btn sidenav__btn--active';
-  const classNameButtonInactive = 'sidenav__btn';
-  const classNameButtonBorderActive = 'sidenav__btn sidenav__btn--border-left-right sidenav__btn--active';
-  const classNameButtonBorderInactive = 'sidenav__btn sidenav__btn--border-left-right';
-
+// eslint-disable-next-line no-shadow
+export function Sidenav({ openNewTaskPopup }) {
   return (
     <aside className="sidenav">
       <ul className="sidenav__list">
@@ -49,36 +22,7 @@ export function Sidenav(props) {
           <input className="sidenav__search" type="search" placeholder="...to be implemented..." />
         </li>
         <li className="sidenav__list-elem">
-          <p>Show tasks:</p>
-          <div className="sidenav__showTasks">
-            <button
-              className={filters.showTasks === 'all' ? classNameButtonActive : classNameButtonInactive}
-              type="button"
-              name="showTasks"
-              value="all"
-              onClick={handleFilter}
-              onMouseDown={e => e.preventDefault()} /* to remove focus after button is clicked */
-            >All
-            </button>
-            <button
-              className={filters.showTasks === 'open' ? classNameButtonBorderActive : classNameButtonBorderInactive}
-              type="button"
-              name="showTasks"
-              value="open"
-              onClick={handleFilter}
-              onMouseDown={e => e.preventDefault()} /* to remove focus after button is clicked */
-            >Open
-            </button>
-            <button
-              className={filters.showTasks === 'completed' ? classNameButtonActive : classNameButtonInactive}
-              type="button"
-              name="showTasks"
-              value="completed"
-              onClick={handleFilter}
-              onMouseDown={e => e.preventDefault()} /* to remove focus after button is clicked */
-            >Completed
-            </button>
-          </div>
+          <StatusFilter />
         </li>
         <li className="sidenav__list-elem">
           <DateFilter />
@@ -91,15 +35,8 @@ export function Sidenav(props) {
 
 Sidenav.propTypes = {
   openNewTaskPopup: PropTypes.func.isRequired,
-  updateFilters: PropTypes.func.isRequired,
-  filters: PropTypes.shape({
-    showTasks: PropTypes.string,
-    dateRangeStart: PropTypes.string,
-    dateRangeEnd: PropTypes.string,
-  }).isRequired,
 };
 
-export default connect(mapStateToProps, {
-  updateFilters,
+export default connect(null, {
   openNewTaskPopup,
 })(Sidenav);
---
+++ src/client/components/js/StatusFilter.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import {
+  updateFilters,
+  openNewTaskPopup,
+} from '../../actions/index';
+
+const mapStateToProps = state => ({
+  filters: state.filters.filters,
+});
+
+function StatusFilter({
+  filters,
+  // eslint-disable-next-line no-shadow
+  updateFilters,
+}) {
+  const handleFilter = (event) => {
+    // eslint-disable-next-line no-shadow
+    updateFilters(event.target.name, event.target.value);
+
+    axios.put('/api/filters', {
+      ...filters,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const classNameButtonActive = 'sidenav__btn sidenav__btn--active';
+  const classNameButtonInactive = 'sidenav__btn';
+  const classNameButtonBorderActive = 'sidenav__btn sidenav__btn--border-left-right sidenav__btn--active';
+  const classNameButtonBorderInactive = 'sidenav__btn sidenav__btn--border-left-right';
+
+  return (
+    <div className="sidenav__status">
+      <p>Show tasks:</p>
+      <div className="sidenav__showTasks">
+        <button
+          className={filters.showTasks === 'all' ? classNameButtonActive : classNameButtonInactive}
+          type="button"
+          name="showTasks"
+          value="all"
+          onClick={handleFilter}
+          onMouseDown={e => e.preventDefault()} /* to remove focus after button is clicked */
+        >All
+        </button>
+        <button
+          className={filters.showTasks === 'open' ? classNameButtonBorderActive : classNameButtonBorderInactive}
+          type="button"
+          name="showTasks"
+          value="open"
+          onClick={handleFilter}
+          onMouseDown={e => e.preventDefault()} /* to remove focus after button is clicked */
+        >Open
+        </button>
+        <button
+          className={filters.showTasks === 'completed' ? classNameButtonActive : classNameButtonInactive}
+          type="button"
+          name="showTasks"
+          value="completed"
+          onClick={handleFilter}
+          onMouseDown={e => e.preventDefault()} /* to remove focus after button is clicked */
+        >Completed
+        </button>
+      </div>
+    </div>
+  );
+}
+
+StatusFilter.propTypes = {
+  updateFilters: PropTypes.func.isRequired,
+  filters: PropTypes.shape({
+    showTasks: PropTypes.string,
+    dateRangeStart: PropTypes.string,
+    dateRangeEnd: PropTypes.string,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps, {
+  updateFilters,
+  openNewTaskPopup,
+})(StatusFilter);
