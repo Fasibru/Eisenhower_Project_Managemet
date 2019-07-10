fd7f8daee61dfb5dddf1eaf0a7f2738c2909450c
Fabian Brundke <fabian_brundke@gmx.net>
2019-07-10 20:47:12 +0200

    Worked on styling of components. Mainly made only Main component scrollable.

--- src/client/components/js/Main.js
+++ src/client/components/js/Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../scss/Main.scss';
@@ -65,12 +66,71 @@ function Main({ filteredTasks }) {
   const categorizedFilteredTasks = categorizeFilteredTasks(
     filteredTasks,
   );
+
+  // eslint-disable-next-line no-underscore-dangle
+  const _sumDivHeights = (nodeList) => {
+    const nodeArray = Array.from(nodeList);
+    const numberOfNodes = nodeArray.length;
+
+    // get styles of nodes - each node has same style due to React component
+    const nodeStyle = nodeArray[0].currentStyle || window.getComputedStyle(nodeArray[0]);
+
+    // get margins
+    const marginTop = Number(nodeStyle.marginTop.split('px')[0]);
+    const marginBottom = Number(nodeStyle.marginBottom.split('px')[0]);
+
+    // calculate margin size without overlap
+    let marginOffset;
+    if (numberOfNodes === 1) {
+      marginOffset = marginTop + marginBottom;
+    } else {
+      marginOffset = numberOfNodes
+        * (marginTop + marginBottom)
+        - (numberOfNodes - 1) * marginTop;
+    }
+
+    // calculate necessary height of whole div container
+    const sumDivHeights = nodeArray.reduce(
+      (offsetHeight, node) => offsetHeight + node.offsetHeight, 0,
+    ) + marginOffset;
+
+    return sumDivHeights;
+  };
+
+  // eslint-disable-next-line no-underscore-dangle
+  const _getMaxContainerDivHeight = (divNodes) => {
+    // get only div containers that have tasks in them
+    const nonEmptyDivNodes = divNodes.filter(node => node.childNodes.length !== 0);
+
+    // get max height of div containers
+    const maxHeight = nonEmptyDivNodes.reduce(
+      (max, node) => (
+        _sumDivHeights(node.childNodes) > max ? _sumDivHeights(node.childNodes) : max), 0,
+    );
+    return maxHeight;
+  };
+
+  const setContainerDivHeight = () => {
+    // calculate height of main div container based on scrolling
+    const height = document.getElementById('main').scrollHeight + document.getElementById('main').scrollTop;
+
+    // get direct child div containers of main div container
+    const divs = document.getElementsByClassName('main__column-category');
+    const divsArray = Array.from(divs);
+
+    // change height of child div containers to extend css border to bottom when scrolled
+    divsArray.forEach((div) => {
+      div.style.maxHeight = _getMaxContainerDivHeight(divsArray) + 'px';
+      div.style.height = height;
+    });
+  };
+
   return (
-    <main className="main">
-      <div className="column-category">{categorizedFilteredTasks.TasksCatA}</div>
-      <div className="column-category">{categorizedFilteredTasks.TasksCatB}</div>
-      <div className="column-category">{categorizedFilteredTasks.TasksCatC}</div>
-      <div className="column-category">{categorizedFilteredTasks.TasksCatD}</div>
+    <main className="main" id="main" onScroll={setContainerDivHeight}>
+      <div className="main__column-category" id="main__column-category">{categorizedFilteredTasks.TasksCatA}</div>
+      <div className="main__column-category">{categorizedFilteredTasks.TasksCatB}</div>
+      <div className="main__column-category">{categorizedFilteredTasks.TasksCatC}</div>
+      <div className="main__column-category">{categorizedFilteredTasks.TasksCatD}</div>
     </main>
   );
 }
