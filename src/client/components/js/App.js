fd050c93fcca9f8cf4b8a22be74a430f942d51cb
Fabian Brundke <fabian_brundke@gmx.net>
2019-08-01 23:42:19 +0200

    Minor cleanup in App

--- src/client/components/js/App.js
+++ src/client/components/js/App.js
@@ -1,6 +1,5 @@
-import React, { Component, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getUserTasks } from '../../actions/actionsTasks';
 import { getUserFilters } from '../../actions/actionsFilters';
