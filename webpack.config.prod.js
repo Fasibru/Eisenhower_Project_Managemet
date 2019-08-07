c5bde0c95782267dc3f4d70f29629a8a67cdeb4d
Fabian Brundke <fabian_brundke@gmx.net>
2019-08-07 22:01:13 +0200

    Fixed they way how clean-webpack-plugin gets required as prod dependency compared to dev dependency.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
