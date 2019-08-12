fd079cc73ce7ae545840eb61a735c3384cf29a1c
Fabian Brundke <fabian_brundke@gmx.net>
2019-08-12 21:54:31 +0200

    Implemented types in NewTask

--- src/client/components/js/NewTask.tsx
+++ src/client/components/js/NewTask.tsx
@@ -1,47 +1,59 @@
 import axios from 'axios';
-import React from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import {
   closeNewTaskPopup,
-  storeNewTaskFormChange,
   saveNewTask,
+  storeNewTaskFormChange,
 } from '../../actions/actionsTasks';
 
 import '../scss/NewTask.scss';
 import '../scss/TaskForm.scss';
 
-import TaskFormTitle from './TaskFormTitle';
-import TaskFormDescription from './TaskFormDescription';
 import TaskFormCategory from './TaskFormCategory';
+import TaskFormDescription from './TaskFormDescription';
+import TaskFormTitle from './TaskFormTitle';
 
-const mapStateToProps = state => ({
+import { NewTaskType, Store, TaskType } from '../../../types/storeTypes';
+import { TaskActionsTypes } from '../../../types/taskActionTypes';
+
+interface NewTaskProps {
+  newTask: NewTaskType;
+  userId: string;
+  closeNewTaskPopup(): TaskActionsTypes;
+  saveNewTask(task: TaskType): TaskActionsTypes;
+  storeNewTaskFormChange(name: string, value: boolean | string): TaskActionsTypes;
+}
+
+const mapStateToProps = (state: Store) => ({
   newTask: state.tasks.newTask,
   userId: state.user.userId,
 });
 
-export function NewTask(props) {
-  const {
-    /* eslint-disable no-shadow */
-    closeNewTaskPopup,
-    storeNewTaskFormChange,
-    saveNewTask,
-    newTask,
-    userId,
-    /* eslint-enable no-shadow */
-  } = props;
+export const NewTask: React.FC<NewTaskProps> = ({
+  // tslint:disable: no-shadowed-variable
+  newTask,
+  userId,
+  closeNewTaskPopup,
+  saveNewTask,
+  storeNewTaskFormChange,
+  // tslint:enable: no-shadowed-variable
+}) => {
 
-  const handleChange = (event) => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>,
+    ) => {
     // reflect form changes in newTask state
     if (event.target.name === 'completed') {
-      storeNewTaskFormChange(event.target.name, event.target.checked);
+      storeNewTaskFormChange(event.target.name, (event.target as HTMLInputElement).checked);
     } else {
       storeNewTaskFormChange(event.target.name, event.target.value);
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     axios.post('/api/task', { ...newTask, members: [userId] })
       .then((res) => {
@@ -95,19 +107,19 @@ export function NewTask(props) {
   );
 }
 
-NewTask.propTypes = {
-  closeNewTaskPopup: PropTypes.func.isRequired,
-  storeNewTaskFormChange: PropTypes.func.isRequired,
-  saveNewTask: PropTypes.func.isRequired,
-  newTask: PropTypes.shape({
-    title: PropTypes.string,
-    description: PropTypes.string,
-    category: PropTypes.oneOf(['A', 'B', 'C', 'D']),
-    rank: PropTypes.number,
-    _id: PropTypes.string,
-  }).isRequired,
-  userId: PropTypes.string.isRequired,
-};
+// NewTask.propTypes = {
+//   closeNewTaskPopup: PropTypes.func.isRequired,
+//   storeNewTaskFormChange: PropTypes.func.isRequired,
+//   saveNewTask: PropTypes.func.isRequired,
+//   newTask: PropTypes.shape({
+//     title: PropTypes.string,
+//     description: PropTypes.string,
+//     category: PropTypes.oneOf(['A', 'B', 'C', 'D']),
+//     rank: PropTypes.number,
+//     _id: PropTypes.string,
+//   }).isRequired,
+//   userId: PropTypes.string.isRequired,
+// };
 
 export default connect(mapStateToProps, {
   closeNewTaskPopup,
