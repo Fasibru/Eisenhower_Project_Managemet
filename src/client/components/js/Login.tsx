5b2f7abc94fb7f86b23b8b4c17877c45fa864cd1
Fabian Brundke <fabian_brundke@gmx.net>
2019-08-12 22:55:35 +0200

    Reduced tslint warnings in Login

--- src/client/components/js/Login.tsx
+++ src/client/components/js/Login.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
+// tslint:disable-next-line: import-name
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import React, { useEffect } from 'react';
 // import PropTypes from 'prop-types';
 
 import {
