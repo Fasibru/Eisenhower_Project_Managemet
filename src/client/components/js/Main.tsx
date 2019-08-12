7c7d30d64c040690c5f6595aa67df9cc623d8b89
Fabian Brundke <fabian_brundke@gmx.net>
2019-08-12 22:42:17 +0200

    Implemented types in Main component

--- src/client/components/js/Main.tsx
+++ src/client/components/js/Main.tsx
@@ -1,12 +1,33 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+// import PropTypes from 'prop-types';
+
 import '../scss/Main.scss';
 import '../scss/Task.scss';
+
 import Task from './Task';
 
-const categorizeFilteredTasks = (filteredTasks) => {
-  const categorizedFilteredTasks = {
+import { TaskType } from '../../../types/storeTypes';
+
+interface TasksCatProps {
+  className: string;
+  key: string;
+  task: TaskType;
+}
+
+interface CategorizeFilteredTasks {
+  TasksCatA: Array<React.ReactElement<TasksCatProps>>;
+  TasksCatB: Array<React.ReactElement<TasksCatProps>>;
+  TasksCatC: Array<React.ReactElement<TasksCatProps>>;
+  TasksCatD: Array<React.ReactElement<TasksCatProps>>;
+}
+
+interface MainProps {
+  filteredTasks: TaskType[];
+}
+
+const categorizeFilteredTasks = (filteredTasks: TaskType[]) => {
+  const categorizedFilteredTasks: CategorizeFilteredTasks = {
     TasksCatA: [],
     TasksCatB: [],
     TasksCatC: [],
@@ -62,7 +83,7 @@ const categorizeFilteredTasks = (filteredTasks) => {
   return categorizedFilteredTasks;
 };
 
-function Main({ filteredTasks }) {
+const Main: React.FC<MainProps> = ({ filteredTasks }) => {
   const categorizedFilteredTasks = categorizeFilteredTasks(
     filteredTasks,
   );
@@ -70,27 +91,42 @@ function Main({ filteredTasks }) {
   return (
     <main className="main" id="main">
       <div className="main__column-category main__column-A">
-        <span className="main__column-header">Important and Urgent ({categorizedFilteredTasks.TasksCatA.length})</span>
+        <span
+          className="main__column-header"
+        >
+          Important and Urgent ({categorizedFilteredTasks.TasksCatA.length})
+        </span>
         {categorizedFilteredTasks.TasksCatA}
       </div>
       <div className="main__column-category main__column-B">
-        <span className="main__column-header">Important ({categorizedFilteredTasks.TasksCatB.length})</span>
+        <span
+          className="main__column-header"
+        >
+          Important ({categorizedFilteredTasks.TasksCatB.length})
+        </span>
         {categorizedFilteredTasks.TasksCatB}
       </div>
       <div className="main__column-category main__column-C">
-        <span className="main__column-header">Urgent ({categorizedFilteredTasks.TasksCatC.length})</span>
+        <span
+          className="main__column-header">
+          Urgent ({categorizedFilteredTasks.TasksCatC.length})
+        </span>
         {categorizedFilteredTasks.TasksCatC}
       </div>
       <div className="main__column-category main__column-D">
-        <span className="main__column-header">Not Important and not Urgent ({categorizedFilteredTasks.TasksCatD.length})</span>
+        <span
+          className="main__column-header"
+        >
+          Not Important and not Urgent ({categorizedFilteredTasks.TasksCatD.length})
+        </span>
         {categorizedFilteredTasks.TasksCatD}
       </div>
     </main>
   );
 }
 
-Main.propTypes = {
-  filteredTasks: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
+// Main.propTypes = {
+//   filteredTasks: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
 
 export default Main;
